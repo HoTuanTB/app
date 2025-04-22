f9f8bc33ad95337dd1eab8d698719420d7b068c3
Ho Tuan <hotuanit00gmail.com>
2025-04-22 17:26:04 +0700

    video demo

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -209,7 +209,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <iframe width="100%" height="400" src="https://www.youtube.com/embed/eL_TtPCGttY"
+                    <iframe width="100%" height="400" src="https://www.youtube.com/embed/91N1qWMWvDY"
                             frameborder="0"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen></iframe>
